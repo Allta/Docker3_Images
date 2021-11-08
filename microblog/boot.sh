6d8b028cf201e8076734fbd1f6d463c3493531db
Allta <51991304+Allta@users.noreply.github.com>
2021-11-08 13:25:17 +0100

    Update boot.sh

--- microblog/boot.sh
+++ microblog/boot.sh
@@ -2,7 +2,7 @@
 
 # cette partie permet de faire varier l'environnement du container
 set -e
-if [ "$CONTEXT" = 'DEV' ]; then
+if [ "$CONTEXT" == 'DEV' ]; then
     echo "Running Development Server"
     FLASK_ENV=development exec flask run -h 0.0.0.0
 else
